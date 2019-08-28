a2b6741141d2863a12b150d56992760b5f9060e5
lingjingxue <lingjingxue@sina.com>
2019-08-28 16:24:02 +0800

    js支持

---
+++ ExcelTool/Tool/XToolJavaScript.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using NPOI.SS.UserModel;        //NPOI
+using NPOI.XSSF.UserModel;      //NPOI
+using System.Threading;
+using static ExcelTool.XGlobal;
+
+namespace ExcelTool
+{
+    public static partial class XTool
+    {
+        public static StringBuilder sbJsonJavaScript = new StringBuilder();
+
+        public static void TransferClassClientJavaScript()
+        {
+            if (true)
+            {
+                string path_class = PathClass + "\\" + "ConfClientConf.js";
+                XGlobal.DeleteFile(path_class);
+                FileStream fs_class = new FileStream(path_class, FileMode.OpenOrCreate);
+                StreamWriter sw_class = new StreamWriter(fs_class);
+                sw_class.Write(sbJsonJavaScript);
+                sw_class.Close();
+            }
+            if (true)
+            {
+                var sbenum = new StringBuilder();
+                foreach (var kvp in DictDictEnums)
+                {
+                    sbenum.Append($"var ConfEnum{kvp.Key} = {{\r\n");
+                    long min = 0, max = 0;
+                    foreach (var kvp2 in kvp.Value)
+                    {
+                        sbenum.Append($"\t{kvp2.Key}: {kvp2.Value},\r\n");
+                        long.TryParse(kvp2.Value, out long tempmax);
+                        min = Math.Min(min, tempmax);
+                        max = Math.Max(max, tempmax);
+                    }
+                    sbenum.Append($"}};\r\n");
+                }
+                foreach (var kvp in DictDictEnums)
+                {
+                    sbenum.Append($"var ConfEnumMap{kvp.Key} = {{\r\n");
+                    long min = 0, max = 0;
+                    foreach (var kvp2 in kvp.Value)
+                    {
+                        sbenum.Append($"\t{kvp2.Value}: \"{kvp2.Key}\",\r\n");
+                        long.TryParse(kvp2.Value, out long tempmax);
+                        min = Math.Min(min, tempmax);
+                        max = Math.Max(max, tempmax);
+                    }
+                    sbenum.Append($"}};\r\n");
+                }
+
+                string path_class = PathClass + "\\" + "ConfClientEnum.js";
+                XGlobal.DeleteFile(path_class);
+                FileStream fs_class = new FileStream(path_class, FileMode.OpenOrCreate);
+                StreamWriter sw_class = new StreamWriter(fs_class);
+                sw_class.Write(sbenum);
+                sw_class.Close();
+            }
+        }
+    }
+}
---
+++ ProtocolTool/JavaScriptConverter.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Text;
+using System;
+using Microsoft.Office.Interop.Word;
+
+namespace ProtocolTool
+{
+    partial class Program
+    {
+        //ClassTypeScript
+        public static readonly string Filepath_ClassJavaScript = @"DispatcherPtl.js";
+
+        public static readonly string Template_ClassJavaScript = @"
+
+
+function DispatcherProtocol(Msg) {
+    var ProtocolId = Msg._protocol;
+    switch (ProtocolId) {
+
+$Class1$
+
+        default:
+    }
+}
+
+
+var MapProtocolId = {
+$Class31$
+    properties: {
+$Class32$
+    }
+};
+
+";
+
+        /// <summary>
+        /// 生成结构体文件 JavaScript
+        /// </summary>
+        private static void ProtocolConverterClassJavaScript()
+        {
+            Show("ProtocolConverterClassJavaScript");
+            var sb1 = new StringBuilder();
+            var sb2 = new StringBuilder();
+            var sb31 = new StringBuilder();
+            var sb32 = new StringBuilder();
+
+            foreach (var kvp in DictClass)
+            {
+                if (kvp.Value.ClassType == 0)
+                {
+                    if (kvp.Value.Name.StartsWith("G2C") || kvp.Value.Name.StartsWith("L2C"))
+                    {
+                        if (kvp.Value.Desc != "")
+                        {
+                            sb1.Append($"        // {kvp.Value.Desc}\r\n");
+                        }
+                        sb1.Append($"        case MapProtocolId.{kvp.Value.NameId}: On_{kvp.Value.Name}(Msg); break;\r\n");
+                    }
+
+                    //sb2.Append($"var {kvp.Value.NameId} = {kvp.Value.Id} // {kvp.Value.Desc}\r\n");
+
+                    sb31.Append($"    {kvp.Value.NameId}: {kvp.Value.Id}, // {kvp.Value.Desc}\r\n");
+
+                    sb32.Append($"        {kvp.Value.Id}: {{ name: \"{kvp.Value.NameId}\", value: {kvp.Value.Id}, desc: \"{kvp.Value.Desc}\" }},\r\n");
+                }
+            }
+            string txt = Template_ClassJavaScript;
+            FileStream fs = new FileStream(PathCurrent + Filepath_ClassJavaScript, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            txt = txt.Replace("$Class1$", sb1.ToString());
+            //txt = txt.Replace("$Class2$", sb2.ToString());
+            txt = txt.Replace("$Class31$", sb31.ToString());
+            txt = txt.Replace("$Class32$", sb32.ToString());
+            sw.Write(txt);
+            sw.Close();
+            Show("输出文件：" + Filepath_ClassJavaScript);
+        }
+
+    }
+}
